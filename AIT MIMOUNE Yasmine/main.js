38ede7b7b3cfb2ea3d49055c1b4cb2313d0c86a9
YasmineAit <119767024+YasmineAit@users.noreply.github.com>
2024-05-11 13:50:44 +0100

    Ait Mimoune Yasmine

---
+++ AIT MIMOUNE Yasmine/main.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector("ul");
+        let bar = document.querySelector(".bar .fa-bars");
+
+        bar.addEventListener("click", function(){
+            menu.classList.toggle("show")
+        })
